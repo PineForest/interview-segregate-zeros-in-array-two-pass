637a4d7badd00eb83c0f1c61d484aeff09f367df
PineForest <davidkwilliams@yahoo.com>
2016-03-29 20:55:13 -0700

    Added LGPL.

--- src/test/java/interview/practice/SegregateArrayZerosTest.java
+++ src/test/java/interview/practice/SegregateArrayZerosTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright © 2016  David Williams
+ *
+ * This file is part of the interview-segregate-zeros-in-array-two-pass project.
+ *
+ * interview-segregate-zeros-in-array-two-pass is free software: you can redistribute it and/or modify it under the terms of the
+ * Lesser GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * interview-segregate-zeros-in-array-two-pass is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License along with interview-segregate-zeros-in-array-two-pass.
+ * If not, see <a href="http://www.gnu.org/licenses/">www.gnu.org/licenses/</a>.
+ */
+
 package interview.practice;
 
 import junit.framework.Test;
